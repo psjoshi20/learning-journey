c13b846fc00024fcf0303f4d4bf53b17ced89054
psjoshi20 <psjoshi20@gmail.com>
2019-12-03 00:47:53 +0800

    final working commit

--- techladies/psj-express/index.js
+++ techladies/psj-express/index.js
@@ -1,6 +1,10 @@
 const express =require("express");
 const app =express(); //creating instance of express
 
+app.get('/',(req,res) => { 
+   res.send("Hello Techladies , welcome to simple Express APP exercise");
+});
 // app listen on port3000
 app.listen(3000,()=>
-     console.log("psj-express app listening on port =3000"));
+     console.log("psj-express app listening on port =3000")
+     );
