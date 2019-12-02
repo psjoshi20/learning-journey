61dd4474b1dc7234b124a105b822ba5eb450c50e
psjoshi20 <psjoshi20@gmail.com>
2019-12-03 00:14:24 +0800

    initial commit for psj-express app

---
+++ techladies/psj-express/index.js
@@ -0,0 +1,6 @@
+const express =require("express");
+const app =express(); //creating instance of express
+
+// app listen on port3000
+app.listen(3000,()=>
+     console.log("psj-express app listening on port =3000"));
