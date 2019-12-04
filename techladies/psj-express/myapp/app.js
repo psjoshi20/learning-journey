0ceaa0410591beb14bdea7fbd7be2d9bb6369a3d
psjoshi20 <psjoshi20@gmail.com>
2019-12-05 00:40:25 +0800

    edited app.js

--- techladies/psj-express/myapp/app.js
+++ techladies/psj-express/myapp/app.js
@@ -4,7 +4,9 @@ const port = 3000
 
 app.get('/', (req, res) => {
   const name = req.query.name || 'elisha'
-  res.send('Hello ' + name + '!')
+  res.send('Hello this is a basic Nodejs app ,' + name + '!')
+
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+console.log('test message by me ****') 
--- techladies/psj-express/myapp2/routes/index.js
+++ techladies/psj-express/myapp2/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.send('myapp2- express app , aweb server using Express Generator');
   res.render('index', { title: 'Express' });
+ 
 });
 
 module.exports = router;
