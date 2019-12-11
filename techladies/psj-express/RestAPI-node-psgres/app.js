2e023f317f34b5a2a8f66f1d6245861342c00d16
psjoshi20 <psjoshi20@gmail.com>
2019-12-11 18:29:02 +0800

    debugging the function

--- techladies/psj-express/RestAPI-node-psgres/app.js
+++ techladies/psj-express/RestAPI-node-psgres/app.js
@@ -2,27 +2,29 @@ const http =require('http');//import http from 'http'
 const express = require('express');//import express from 'express'
 const logger = require('morgan');//import logger from 'morgan';
 const bodyParser = require('body-parser');//import bodyParser from 'body-parser';
-import routes from './server/routes'; 
-// Set up the express app
+
 const hostname = '127.0.0.1'; 
 const port = 3000;
 const app = express();
+// setup express app server 
 const server = http.createServer(app); 
+app.use(logger('dev'));
+
+var userRouter =require('./servers/controllers/routes/user');
+app.use('/users', userRouter);
 routes(app);
 
 // Log requests to the console.
-app.use(logger('dev'));
-
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
+  message: 'Welcome to the default API route.',
 }));
 
 server.listen(port, hostname, () => { console.log(`Server running at http://${hostname}:${port}/`); });
 
 module.exports = app;
  
+//require('./server/routes')(app);
